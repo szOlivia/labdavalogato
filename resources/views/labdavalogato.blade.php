3e96f116327973b67a8d4685e294c020e1af42d9
szOlivia <szaboolivia@elg-bp.edu.hu>
2023-02-19 17:21:36 +0100

    labdavalogato.blade.php urlap

--- resources/views/labdavalogato.blade.php
+++ resources/views/labdavalogato.blade.php
@@ -7,6 +7,33 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Labda szín válogató</h1>
+    <form>
+        <label> Hány labdát szeretne válogatni?</label><br>
+        <input type="number" name="db" id="db" minlength="1" maxlength="10"><br>
+         <br>
+
+        <label >Melyik színból szeretne legalabb 1db-ot?</label> <br>
+        <label for="labda_szin">Labda szín:</label>
+        <select id="labda_szin" name="labda_szin" size="3">
+            <option value="feher">Fehér</option>
+            <option value="narancs">Narancssárga</option>
+            <option value="kek">Kék</option>
+            <option value="rozsaszin">Rózsaszín</option>
+            <option value="zold">Zöld</option>
+            <option value="citrom">Citromsárga</option>
+        </select> <br>
+        <br>
+        <label>Addig menjen a folyamat, ameddig ki nem gurul az a színű labda, amit kiválasztott?</label> 
+        <br>
+        <input type="radio" name="i_n" id=""> Igen <br>
+        <input type="radio" name="i_n" id=""> Nem 
+        <br> <br>
+
+        <label> Kitöltött adatok beküldése:</label> <input type="submit" value="Beküldés">
+
+    </form>
+
     
 </body>
 </html>
