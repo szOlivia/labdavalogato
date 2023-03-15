ba25a9195d5f7e6608e05bef45fcbe78e6982fa0
szOlivia <szaboolivia@elg-bp.edu.hu>
2023-03-15 15:16:51 +0100

    szintek atirasa

--- resources/views/labdavalogato.blade.php
+++ resources/views/labdavalogato.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="hu">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,18 +10,16 @@
     <h1>Labda szín válogató</h1>
     <form>
         <label> Hány labdát szeretne válogatni?</label><br>
-        <input type="number" name="db" id="db" minlength="1" maxlength="10"><br>
+        <input type="number" name="db" id="db" minlength="1" maxlength="6"><br>
          <br>
 
         <label >Melyik színból szeretne legalabb 1db-ot?</label> <br>
         <label for="labda_szin">Labda szín:</label>
         <select id="labda_szin" name="labda_szin" size="3">
-            <option value="feher">Fehér</option>
-            <option value="narancs">Narancssárga</option>
+            <option value="piros">Piros</option>
             <option value="kek">Kék</option>
-            <option value="rozsaszin">Rózsaszín</option>
             <option value="zold">Zöld</option>
-            <option value="citrom">Citromsárga</option>
+            
         </select> <br>
         <br>
         <label>Addig menjen a folyamat, ameddig ki nem gurul az a színű labda, amit kiválasztott?</label> 
