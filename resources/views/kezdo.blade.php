10720e354577cf5f7c2a011bf8ee2623cc957daf
szOlivia <szaboolivia@elg-bp.edu.hu>
2023-03-16 20:44:40 +0100

    bootstrap igazitása

--- resources/views/kezdo.blade.php
+++ resources/views/kezdo.blade.php
@@ -5,18 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
-<!-- Latest compiled and minified CSS -->
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-<!-- Latest compiled JavaScript -->
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <title>Kezdő oldal</title>
 </head>
 <body style="background-image: url({{URL('./img/kezdohatter.jpg')}})">
-    <h1 class="p-5 text-white">Válogassunk labda színeket!</h1>
+    
+    <h1 class="p-5 text-white" >Válogassunk labda színeket!</h1>
+    
     <div class="ps-5">
-        <a class="btn btn-primary" href="./labdavalogato">Tovább</a>
+         <a class="btn btn-primary" href="./labdavalogato">Tovább a szín válogághoz</a> 
     </div>
     
 </body>
