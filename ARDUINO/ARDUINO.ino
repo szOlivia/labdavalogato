0fbc5ae79eeab497ffa854537415039ef35fa44e
szOlivia <szaboolivia@elg-bp.edu.hu>
2023-04-13 17:13:50 +0200

    2db_servo_matejovics_mate

--- ARDUINO/ARDUINO.ino
+++ ARDUINO/ARDUINO.ino
@@ -5,12 +5,18 @@
 // színérzékelőhöz
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
+//szervo
+#include <Servo.h>
+
 
 // Set WiFi credentials
 #define WIFI_SSID "Elso_Sor"
+
 #define WIFI_PASS "12345678"
 
-
+//első szervó poziciója
+int pos = 0;
+int pos2 = 0;
 
 // Create a new web server
 ESP8266WebServer webserver(80);
@@ -23,11 +29,19 @@ HTTPClient httpClient;
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
 
+Servo servo1;
+Servo servo2;
+
+
 void setup()
 {
   // Setup serial port
   Serial.begin(115200);
   Serial.println();
+
+  //szervo
+  servo1.attach(D6);
+  servo2.attach(D7);
 
   //Begin WiFi
   WiFi.begin(WIFI_SSID, WIFI_PASS);
@@ -54,8 +68,11 @@ void setup()
 // Listen for HTTP requests
 void loop(void){ 
   webserver.handleClient(); 
+  
 
+  mozgatSzinVizsgalatra();
 
+  mozgatCsuszda();
 
   //színérzékelés
    float red, green, blue;
@@ -68,7 +85,9 @@ void loop(void){
   //Serial.print("\tG:\t"); Serial.print(int(green));
   //Serial.print("\tB:\t"); Serial.print(int(blue));
 
-  szinMeres("red","green","blue");
+  //szinMeres(String(red),String(green),String(blue));
+  pos=0;
+  pos2=0;
   delay(3000);
 }
 
@@ -93,6 +112,22 @@ void szinMeres(String r, String g, String b){
   Serial.println("Tartalom:");
   Serial.println(content);
 }
+
+
+ void mozgatSzinVizsgalatra(){
+ for(pos=pos;pos<=70;pos+=1){
+   servo1.write(pos);
+   delay(15);
+ }
+} 
+
+void mozgatCsuszda(){
+ for(pos2=pos2;pos2<=70;pos2+=1){
+   servo2.write(pos2);
+   delay(15);
+ }
+}
+
 /*
 void mintaKuldes(float tavolsag, String nev){
   String data = "";
@@ -104,5 +139,3 @@ void mintaKuldes(float tavolsag, String nev){
   Serial.println("Tartalom:");
   Serial.println(content);
 }*/
-
-
